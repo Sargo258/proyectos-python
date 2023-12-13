942a0535e6242ef3f2f50992c91e315c1c78375a
santiago <97708495+Sargo258@users.noreply.github.com>
2023-12-13 08:24:48 -0500

    Add files via upload

---
+++ boletas/principal.py
@@ -0,0 +1,36 @@
+import Numeros
+
+
+def preguntar():
+
+    print("Bienvenido a Farmacia Python")
+
+    while True:
+        print("[P] - Perfumeria\n[F] - Farmacia\n[C] - Cosmetica")
+        try:
+            mi_rubro = input("Elija su rubro: ").upper()
+            ["P", "F", "C"].index(mi_rubro)
+        except ValueError:
+            print("Esa no es una opcion valida")
+        else:
+            break
+
+    Numeros.decorador(mi_rubro)
+
+
+def inicio():
+
+    while True:
+        preguntar()
+        try:
+            otro_turno = input("Quieres sacar otro turno? [S] [N]: ").upper()
+            ["S", "N"].index(otro_turno)
+        except ValueError:
+            print("Esa no es una opcion valida")
+        else:
+            if otro_turno == "N":
+                print("Gracas por su visita")
+                break
+
+
+inicio()
